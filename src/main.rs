6f94bca7f0127aaccb384d62879ac29b7fa940ee
drifterz13 <mixmyxxe@gmail.com>
2025-01-14 22:48:54 +0700

    feat: get commit stats

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::path::Path;
+
+use gitty::core::{Commit, GitLogger};
+
+fn main() {
+    println!("Running gitty...");
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        panic!("Missing arguments.")
+    }
+
+    let repo_path = Path::new(&args[1])
+        .to_str()
+        .expect("Repo path is missing.")
+        .to_string();
+
+    let total_commits = Commit::get_total_commits(&repo_path).unwrap();
+    println!("total commits = {total_commits}");
+
+    let batch_size = 100;
+    let total_batch = total_commits.div_ceil(batch_size);
+
+    println!("total batch = {total_batch}");
+    let mut commits: Vec<Commit> = vec![];
+
+    for n in 0..total_batch {
+        let commit_logs = GitLogger::new(repo_path.clone())
+            .skip(n * 100)
+            .max_count(batch_size)
+            .run()
+            .unwrap();
+
+        for commit_log in commit_logs {
+            commits.push(Commit::from(commit_log));
+        }
+    }
+
+    let commits_by_owner = Commit::count_by_owner(commits);
+    println!("commits by owner = {:#?}", commits_by_owner);
+}
